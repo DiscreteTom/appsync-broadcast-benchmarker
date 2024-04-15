9edf4319c92bbada94b5d746b2e9b421dc52da07
DiscreteTom <discrete_tom@outlook.com>
2024-04-15 01:46:35 +0000

    init script from https://github.com/Chen188/appsync-ws-k6/blob/main/k6_appsync.js

--- script.js
+++ script.js
@@ -1,59 +1,122 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { check } from "k6";
+import encoding from "k6/encoding";
+import ws from "k6/ws";
+import { Rate, Trend } from "k6/metrics";
 
-export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 10,
-  // A string specifying the total duration of the test run.
-  duration: '30s',
+const HTTP_API_HOST = __ENV.HTTP_API_HOST; // e.g. xxxxxxxxxx.appsync-api.us-east-1.amazonaws.com
+const REALTIME_API_HOST = __ENV.REALTIME_API_HOST; // e.g. xxxxxxxxxx.appsync-realtime-api.us-east-1.amazonaws.com
+const API_KEY = __ENV.API_KEY; // e.g. da2-xxx
 
-  // The following section contains configuration options for execution of this
-  // test script in Grafana Cloud.
-  //
-  // See https://grafana.com/docs/grafana-cloud/k6/get-started/run-cloud-tests-from-the-cli/
-  // to learn about authoring and running k6 test scripts in Grafana k6 Cloud.
-  //
-  // cloud: {
-  //   // The ID of the project to which the test is assigned in the k6 Cloud UI.
-  //   // By default tests are executed in default project.
-  //   projectID: "",
-  //   // The name of the test in the k6 Cloud UI.
-  //   // Test runs with the same name will be grouped.
-  //   name: "script.js"
-  // },
-
-  // Uncomment this section to enable the use of Browser API in your tests.
-  //
-  // See https://grafana.com/docs/k6/latest/using-k6-browser/running-browser-tests/ to learn more
-  // about using Browser API in your test scripts.
-  //
-  // scenarios: {
-  //   // The scenario name appears in the result summary, tags, and so on.
-  //   // You can give the scenario any name, as long as each name in the script is unique.
-  //   ui: {
-  //     // Executor is a mandatory parameter for browser-based tests.
-  //     // Shared iterations in this case tells k6 to reuse VUs to execute iterations.
-  //     //
-  //     // See https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ for other executor types.
-  //     executor: 'shared-iterations',
-  //     options: {
-  //       browser: {
-  //         // This is a mandatory parameter that instructs k6 to launch and
-  //         // connect to a chromium-based browser, and use it to run UI-based
-  //         // tests.
-  //         type: 'chromium',
-  //       },
-  //     },
-  //   },
-  // }
+const authorization = {
+  host: HTTP_API_HOST,
+  "x-api-key": API_KEY,
 };
 
-// The function that defines VU logic.
-//
-// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
-// about authoring k6 scripts.
-//
-export default function() {
-  http.get('https://test.k6.io');
-  sleep(1);
+const REALTIME_ENDPOINT = `wss://${REALTIME_API_HOST}:443/graphql?header=${encoding.b64encode(
+  JSON.stringify(authorization)
+)}&payload=e30=`;
+const HTTP_ENDPOINT = `https://${HTTP_API_HOST}/graphql`;
+
+export const ResponseSuccessRate = new Rate("ResponseSuccessRate");
+let ws_resp_delay = new Trend("ws_resp_delay_ms");
+
+export const options = {
+  scenarios: {
+    "appsync-listener": {
+      executor: "ramping-vus",
+      exec: "listener",
+      startVUs: 0,
+      stages: [
+        { duration: "5s", target: 1 },
+        { duration: "5s", target: 1 },
+        { duration: "1s", target: 0 },
+      ],
+      gracefulRampDown: "3s",
+      gracefulStop: "3s",
+    },
+    "appsync-broadcast": {
+      executor: "per-vu-iterations",
+      exec: "broadcast",
+      vus: 1,
+      iterations: 1,
+      startTime: "10s",
+      maxDuration: "30s",
+    },
+  },
+  thresholds: {
+    ResponseSuccessRate: [
+      "rate>0.90", // should be great than 90%
+      { threshold: "rate>0.85", abortOnFail: true }, // stop early if less than 85%
+    ],
+  },
+};
+
+export function listener() {
+  const url = REALTIME_ENDPOINT;
+  const params = {
+    headers: { "Sec-WebSocket-Protocol": "graphql-ws" },
+  };
+
+  const response = ws.connect(url, params, (socket) => {
+    socket.on("open", () => {
+      // console.log("connected");
+      socket.send(JSON.stringify({ type: "connection_init" }));
+    });
+
+    socket.on("message", (msg) => {
+      const e = JSON.parse(msg);
+      if (e.type === "connection_ack") {
+        socket.current.send(
+          JSON.stringify({
+            type: "start",
+            id: `${Date.now()}`,
+            payload: {
+              data: JSON.stringify({
+                query:
+                  "subscription SubscribeToData($name: String!) { subscribe(name: $name) { name data } }",
+                variables: {
+                  name: "TODO", // TODO: channel name
+                },
+              }),
+              extensions: {
+                authorization,
+              },
+            },
+          })
+        );
+      }
+      if (e.type === "data") {
+        // TODO: calculate duration
+        console.log(e.payload.data);
+        ResponseSuccessRate.add(1);
+        // ws_resp_delay.add(duration);
+      }
+    });
+
+    socket.on("close", () => console.log("disconnected"));
+    socket.on("error", on_error);
+  });
+
+  check(response, { "status is 101": (r) => r && r.status === 101 });
+}
+
+export function broadcast() {
+  let res = http.post(
+    HTTP_ENDPOINT,
+    JSON.stringify({
+      query:
+        "mutation PublishData($name: String!, $data: AWSJSON!) { publish(name: $name, data: $data) { name data } }",
+      variables: {
+        name: "TODO", // TODO: channel name
+        data: `${Date.now()}`,
+      },
+    }),
+    {
+      headers: {
+        "x-api-key": authorization["x-api-key"],
+      },
+    }
+  );
+  //  console.log('broadcast resp: ' + res.body);
 }
